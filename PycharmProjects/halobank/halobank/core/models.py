ed2ac297afa9bd6772223901c448710050c63ad5
ErrorIt77 <132151623+ItAgain@users.noreply.github.com>
2024-04-11 21:18:18 +0300

    Delete PycharmProjects/halobank directory

--- PycharmProjects/halobank/halobank/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Operation(models.Model):
-    card_id = models.IntegerField()
-    amount = models.IntegerField()
-    shop = models.CharField(max_length=256)
-    date = models.DateTimeField()
